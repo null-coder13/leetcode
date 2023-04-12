96e6f4efa1e4c1ecfe51f0f9a3f1ec81c2e6ffa9
Tom <tgadacy@gmail.com>
2023-04-12 19:21:32 -0400

    Update day 10

--- SeventyFive/DayTen.cs
+++ SeventyFive/DayTen.cs
@@ -21,6 +21,26 @@ namespace SeventyFive
             return cached[n];
         }
 
+        // # Climbing Stairs
+        public int ClimbStairs(int n)
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+            if (n == 2) return 2;
+
+            int[] cached = new int[n + 1];
+            cached[0] = 0;
+            cached[1] = 1;
+            cached[2] = 2;
+
+            for (int i = 3; i <= n; i++)
+            {
+                cached[i] = cached[i - 1] + cached[i - 2];
+            }
+
+            return cached[n];
+        }
+
     }
 
 }
